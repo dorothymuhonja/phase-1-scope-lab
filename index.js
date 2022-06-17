49a7edd2a4a1a245f6eb5cfd9f26af9ff9c34ab3
dorothymuhonja <dorothymuhonja7@gmail.com>
2022-06-17 19:09:36 +0300

    commit tree 3924480b3a159b5b818c798f7fb33aba7b9ed37d

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    customerName = 'BOB';
+}
+
+var bestCustomer
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+    
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'bob?'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'bob'
+}
+
+
